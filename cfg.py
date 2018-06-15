eb40ec1fceea9ca933e70f5f8f5c2a3a6b9d310d
Hui-Yu Huang <purelyvivid@gmail.com>
2018-06-15 21:03:22 +0800

    prepare for torch v0.4

--- cfg.py
+++ cfg.py
@@ -154,7 +154,10 @@ def load_conv(buf, start, conv_model):
     num_w = conv_model.weight.numel()
     num_b = conv_model.bias.numel()
     conv_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w
+    if '0.3' == torch.__version__[:3] or '0.2' == torch.__version__[:3]:
+        conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w #20180615 modified for <= torch v0.3.0 post4
+    else:   
+        conv_model.weight.data.copy_(torch.reshape(torch.from_numpy(buf[start:start+num_w]),(conv_model.weight.shape[0],conv_model.weight.shape[1], conv_model.weight.shape[2],conv_model.weight.shape[3]))); start=start + num_w #20180615 modified for >= torch v0.4.0
     return start
 
 def save_conv(fp, conv_model):
@@ -172,7 +175,10 @@ def load_conv_bn(buf, start, conv_model, bn_model):
     bn_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
     bn_model.running_mean.copy_(torch.from_numpy(buf[start:start+num_b]));  start = start + num_b
     bn_model.running_var.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
+    if '0.3' == torch.__version__[:3] or '0.2' == torch.__version__[:3]:
+        conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w #20180615 modified for <= torch v0.3.0 post4
+    else: 
+        conv_model.weight.data.copy_(torch.reshape(torch.from_numpy(buf[start:start+num_w]),(conv_model.weight.shape[0],conv_model.weight.shape[1], conv_model.weight.shape[2],conv_model.weight.shape[3]))); start=start + num_w #20180615 modified for >= torch v0.4.0 
     return start
 
 def save_conv_bn(fp, conv_model, bn_model):
