f9a6ec57d94b7cd7a582c699994db81b998ec9c3
bklppr@gmail.com <bklppr@gmail.com>
2018-07-14 07:52:43 +0000

    Update

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ max_epochs    = max_batches*batch_size/nsamples+1
 use_cuda      = True
 seed          = int(time.time())
 eps           = 1e-5
-save_interval = 1  # epoches
+save_interval = 10  # epoches
 dot_interval  = 70  # batches
 
 # Test parameters
